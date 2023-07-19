97e8e0e1680eca6d6daf11fea32210841e7afd58
maruthu001 <maruthan.alagar@fwsa.freshworks.com>
2023-07-19 20:05:06 +0530

    Day 08 Completed

--- src/main/java/com/fssa/corejava/day07/practice/RemoveTaskDuplicates.java
+++ src/main/java/com/fssa/corejava/day07/practice/RemoveTaskDuplicates.java
@@ -2,22 +2,15 @@ package com.fssa.corejava.day07.practice;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Objects;
 
 class Task {
 	public String getName() {
 		return name;
 	}
 
-	public void setName(String name) {
-		this.name = name;
-	}
-
 	public String getDeadline() {
 		return deadline;
-	}
-
-	public void setDeadline(String deadline) {
-		this.deadline = deadline;
 	}
 
 	private String name;
@@ -28,31 +21,42 @@ class Task {
 		this.deadline = deadline;
 	}
 
-	@Override
-	public int hashCode() {
-		return name.hashCode() + deadline.hashCode();
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null || getClass() != obj.getClass()) {
+			return false;
+		} else {
+			Task task = (Task) obj;
+			return Objects.equals(name, task.name) && Objects.equals(deadline, task.deadline);
+		}
+
 	}
 
 	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null || getClass() != obj.getClass())
-			return false;
-		Task otherTask = (Task) obj;
-		return name.equals(otherTask.name) && deadline.equals(otherTask.deadline);
+	public int hashCode() {
+		return Objects.hash(name, deadline);
 	}
+
 }
 
 public class RemoveTaskDuplicates {
 	public static void main(String[] args) {
 
 		ArrayList<Task> tasksWithDuplicates = new ArrayList<>();
-		tasksWithDuplicates.add(new Task("Task1", "2023-07-19"));
-		tasksWithDuplicates.add(new Task("Task2", "2023-07-20"));
-		tasksWithDuplicates.add(new Task("Task1", "2023-07-19"));
-		tasksWithDuplicates.add(new Task("Task3", "2023-07-21"));
-		tasksWithDuplicates.add(new Task("Task2", "2023-07-20"));
+
+		Task task1 = new Task("Task1", "2023-07-19");
+		Task task2 = new Task("Task2", "2023-07-20");
+		Task task3 = new Task("Task1", "2023-07-19");
+		Task task4 = new Task("Task3", "2023-07-21");
+		Task task5 = new Task("Task2", "2023-07-20");
+
+		tasksWithDuplicates.add(task1);
+		tasksWithDuplicates.add(task2);
+		tasksWithDuplicates.add(task3);
+		tasksWithDuplicates.add(task4);
+		tasksWithDuplicates.add(task5);
 
 		HashSet<Task> uniqueTasks = new HashSet<>(tasksWithDuplicates);
 
